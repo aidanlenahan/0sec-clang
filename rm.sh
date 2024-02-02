6adb0aa6271a3d562cc55a840971221559016df7
Aidan Lenahan <130784426+aidanlenahan@users.noreply.github.com>
2024-02-02 00:05:12 -0500

    Create rm.sh

---
+++ rm.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+sudo rm /usr/bin/0c
+sudo rm -rf ./0sec-clang/
+echo "0sec-clang from https://github.com/aidanlenahan/0sec-clang was deleted."
+echo "You may delete this script: ./DELETE-ME.sh"
