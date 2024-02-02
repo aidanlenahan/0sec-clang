9582898b2798fdaeeb387cd6cfe547d614f19973
Aidan Lenahan <130784426+aidanlenahan@users.noreply.github.com>
2024-02-01 23:56:42 -0500

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -2,4 +2,4 @@
 #ONLY RUN THIS SCRIPT INSIDE CLONED GIT REPO
 sudo rm /usr/bin/0c
 sudo rm -rf ./0sec-clang/
-echo "cd .." to get out of nonexistent directory.
+echo "'"cd .."'" to get out of nonexistent directory.
