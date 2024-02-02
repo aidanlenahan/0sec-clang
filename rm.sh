2a037e90a0e529d3953bae04434c3681c45cc724
Aidan Lenahan <130784426+aidanlenahan@users.noreply.github.com>
2024-02-02 00:11:23 -0500

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -2,4 +2,4 @@
 sudo rm /usr/bin/0c
 sudo rm -rf ./0sec-clang/
 echo "0sec-clang from https://github.com/aidanlenahan/0sec-clang was deleted."
-echo "You may delete this script: ./DELETE-ME.sh"
+echo "Run this script to remove all files from this Github repository. You may then delete this script after it is run: ./DELETE-ME.sh"
